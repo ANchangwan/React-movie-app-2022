a105e7e3842f0b9be4fb909322ed1c16b629295f
An chang wan <ssdii66@gmail.com>
2022-04-30 16:32:40 +0900

    useEffect 정리

--- react-for-study/src/App.js
+++ react-for-study/src/App.js
@@ -1,10 +1,33 @@
-import Button from "./Button";
+// import Button from "./Button";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [value, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const countUp = () => setValue((prev) => prev + 1);
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+  console.log("리엑트가 rerander 됨");
+  useEffect(() => {
+    console.log("한번만 실행", []);
+  });
+  useEffect(() => {
+    console.log("keyword가 빠뀔 때마다 실행", keyword);
+  }, [keyword]);
+  useEffect(() => {
+    console.log("keyword와 count가 변화할 때마다 변화");
+  }, [value, keyword]);
   return (
     <div>
-      <h1>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="search hear..."
+      />
+      <h1>click : {value}</h1>
+      <button onClick={countUp}>click</button>
     </div>
   );
 }
