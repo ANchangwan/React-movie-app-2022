a839f010ead4b443df9498465c5d3403facc6a44
Anchangwan <56478474+ANchangwan@users.noreply.github.com>
2022-12-22 11:07:47 +0900

    Update Detail.js

--- react-for-study/src/routes/Detail.js
+++ react-for-study/src/routes/Detail.js
@@ -1,18 +1,23 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState, useCallback } from "react";
+import { Movie } from "./../component/Movie";
 
 function Detail() {
   const { id } = useParams();
-  const getMovie = async () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getMovies = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    setMovies([json.data.movie]);
+    setLoading(false);
   };
-  useEffect(() => {
-    getMovie();
-  }, [id]);
-  return <h1>Detail</h1>;
+
+  useEffect(() => getMovies(), []);
+
+  return <div>{loading ? <h1>Loading...</h1> : <Movie movies={movies} />}</div>;
 }
 
 export default Detail;
